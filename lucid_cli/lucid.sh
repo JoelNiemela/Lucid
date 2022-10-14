12c9b8de03faddaa2c94828f9d3256c2d5213f88
JoelNiemela <joel@niemela.se>
2022-10-13 20:25:14 -0500

    Create lucid cli script

---
+++ lucid_cli/lucid.sh
@@ -0,0 +1,28 @@
+SCRIPT_DIR=`dirname $(readlink -f $0)`
+
+if [ -z "$1" ]; then
+	echo "Expected a command."
+	echo "Type 'lucid help' for more information."
+	exit 1
+fi
+
+case "$1" in
+	init)
+		if [ -z "$2" ]; then
+			echo "Expected a name."
+			echo "Type 'lucid help' for more information."
+			exit 1
+		fi
+		if [ ! -d "./$2" ]
+		then
+			cp -R "$SCRIPT_DIR/base_project" "./$2"
+		else
+			echo "Directory $2 already exists."
+			exit 1
+		fi
+		;;
+	*)
+		echo "Unknown command '$1'."
+		exit 1
+		;;
+esac
