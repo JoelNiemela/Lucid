1bac7d942aaa4a7a6fd8159f9c6e2b2fdd246c87
JoelNiemela <joel@niemela.se>
2022-10-14 20:15:18 -0500

    Add MakeFile and update README

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $@"
+    exit 1
+fi
+
 cp -R src/. /usr/local/lib/Lucid
 mkdir -p /usr/local/lib/Lucid/cli
 cp -R cli/base_project/. /usr/local/lib/Lucid/cli/base_project
