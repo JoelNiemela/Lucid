02579f6d35de8a61818865048164667b88e5bfbd
JoelNiemela <joel@niemela.se>
2022-11-12 12:37:23 -0600

    Add access modifier to example project methods

--- example/controllers/post_controller.php
+++ example/controllers/post_controller.php
@@ -4,25 +4,25 @@ require LUCID . 'view.php';
 require ROOT . '/models/Post.php';
 
 class PostController {
-    function index() {
+    public function index() {
         $posts = Post::all();
         view('Post/index.php', ['posts' => $posts]);
     }
 
-    function show(int $post_id) {
+    public function show(int $post_id) {
         $post = Post::find(['post_id' => $post_id]);
         view('Post/show.php', ['post' => $post]);
     }
 
-    function new() {
+    public function new() {
         view('Post/new.php');
     }
 
-    function edit(int $post_id) {
+    public function edit(int $post_id) {
         view('Post/edit.php');
     }
 
-    function create() {
+    public function create() {
         $title = $_POST['title'];
         $text = $_POST['text'];
 
@@ -32,7 +32,7 @@ class PostController {
         header("Location: $post_id");
     }
 
-    function update(int $post_id) {
+    public function update(int $post_id) {
         $title = $_POST['title'];
         $text = $_POST['text'];
 
@@ -42,7 +42,7 @@ class PostController {
         header("Location: ../$post_id");
     }
 
-    function destroy(int $post_id) {
+    public function destroy(int $post_id) {
         Post::delete(['post_id' => $post_id]);
 
         header("Location: ../../post");
